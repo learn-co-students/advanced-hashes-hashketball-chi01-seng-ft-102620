268112066e834fc6ea62cb95e796088425183f69
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-19 03:05:24 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,79 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(name)
+  game_hash.each do |team, info|
+    info[:players].each do |player|
+      if player[:player_name] == name
+        return player[:points]
+      end
+    end
+  end
+end
+
+def shoe_size(name)
+  game_hash.each do |team, info|
+    info[:players].each do |player|
+      if player[:player_name] == name
+        return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team)
+  if team == "Charlotte Hornets" 
+    return game_hash[:away][:colors]
+  else return game_hash[:home][:colors]
+  end
+end
+
+def team_names
+  game_hash.map do |team, info|
+    info[:team_name]
+  end
+end
+
+def player_numbers(x)
+  y = []
+  game_hash.each do |team, info|
+    if info[:team_name] == x 
+      info.each do |key, value|
+        if key == :players
+          value.each do |player|
+          y.push(player[:number])
+          end
+        end
+      end
+    end
+  end
+  return y
+end
+
+def player_stats(name)
+  game_hash.each do |team, info|
+    info.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if name == player[:player_name]
+            return player
+          end
+        end
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, info|
+    info[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
